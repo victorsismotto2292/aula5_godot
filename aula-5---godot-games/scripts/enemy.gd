5562e3ac9417e772a23b1718a94ab731f1f751aa
Vecyn_SisMoTTO <167320276+victorsismotto2292@users.noreply.github.com>
2026-06-05 20:00:38 -0300

    Adicionando script do inimigo
    Adicionando enemy.gd na pasta "Scripts" com o código do Caio.

---
+++ aula-5---godot-games/scripts/enemy.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+const SPEED = 80.0
+const GRAVITY = 800.0
+
+var direction = 1
+
+@onready var floor_left = $FloorLeft # mesmo nome do nó
+@onready var floor_right = $FloorRight
+@onready var anim = $AnimatedSprite2D
+
+func _psyhics_process(delta):
+	# Física para o inimigo detectar o chão
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+		
+		# Inverter ao detectar borda com o mesmo padrão
+		if not floor_left.is_colliding():
+			direction = 1
+		if not floor_right.is_colliding():
+			direction = -1
+		# Aplicando velocidade no eixo x:
+		velocity.x = direction * SPEED
+		anim.flp_h = direction < 0
+	anim.play("walk")
+	
+	move_and_slide()
