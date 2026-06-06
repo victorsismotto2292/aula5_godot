383ac6a82ee22ec812cc70330156bbc801bd76f2
Vecyn_SisMoTTO <167320276+victorsismotto2292@users.noreply.github.com>
2026-06-06 14:37:04 -0300

    Script bugado do inimigo arrumado
    Ok, killzone funcionando e inimigo detectando o chão, só faltam as moedas.

--- aula-5---godot-games/scripts/enemy.gd
+++ aula-5---godot-games/scripts/enemy.gd
@@ -5,7 +5,7 @@ const GRAVITY = 800.0
 
 var direction = 1
 
-@onready var floor_left = $FloorLeft
+@onready var floor_left = $FloorLeft # mesmo nome do nó
 @onready var floor_right = $FloorRight
 @onready var anim = $AnimatedSprite2D
 
@@ -14,15 +14,14 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 		
-	# Inverter ao detectar borda ou parede
-	if is_on_wall() or (is_on_floor() and not floor_left.is_colliding() and direction == -1):
-		direction = 1
-	elif is_on_wall() or (is_on_floor() and not floor_right.is_colliding() and direction == 1):
-		direction = -1
-		
-	# Aplicando velocidade no eixo x:
-	velocity.x = direction * SPEED
-	anim.flip_h = direction > 0
+		# Inverter ao detectar borda com o mesmo padrão
+		if not floor_left.is_colliding():
+			direction = 1
+		if not floor_right.is_colliding():
+			direction = -1
+		# Aplicando velocidade no eixo x:
+		velocity.x = direction * SPEED
+		anim.flip_h = direction > 0
 	anim.play("walk")
 	
 	move_and_slide()
